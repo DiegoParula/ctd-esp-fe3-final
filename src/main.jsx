4c7d69422fc49cf04d13a41b7f5d846b22b72142
Diego Parula <parula27@gmail.com>
2023-09-07 18:21:11 -0300

    rutas

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Routes/Home.jsx'
+import Favs from './Routes/Favs.jsx'
+import Detail from './Routes/Detail.jsx'
+import Contact from './Routes/Contact.jsx'
+import Error from './Components/Error.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />}>
+          <Route path='/home' element={<Home />}/>
+          <Route path='/favs' element={<Favs />}/>
+          <Route path='/details/:id' element={<Detail />}/>
+          <Route path='/contact' element={<Contact />}/>
+          <Route path='*' element={<Error />}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
