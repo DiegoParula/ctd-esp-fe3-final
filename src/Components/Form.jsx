8ecb66f17f1ca27fe477d65e48ea00dbacc102cf
Diego Parula <parula27@gmail.com>
2023-09-08 18:09:20 -0300

    home-detail-formsinvalidar

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 const Form = () => {
   //Aqui deberan implementar el form completo con sus validaciones
+  const [name, setName] = useState()
+  const [mail, setMail] = useState()
+
+
+  const onSubmitForm = (e) => {
+    e.preventDefault()
+    console.log(name + mail)
+  }
 
   return (
     <div>
-      <form>
+      <form onSubmit={onSubmitForm}>
+        <input 
+          type="text"
+          placeholder="Name" 
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input 
+          type="mail" 
+          placeholder="Mail"
+          onChange={(e) => setMail(e.target.value)}
+        />
+
+        <button type='submit'>Send</button>
       </form>
     </div>
   );
--- src/Routes/Detail.jsx
+++ src/Routes/Detail.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useAsyncValue, useParams } from 'react-router-dom'
 
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Detail = () => {
+  const [dentist, setDentist] = useState([])
+  const params = useParams()
+
+  const getDentist = async()=>{
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`)
+    const data = await res.json()
+    setDentist(data)
+    //console.log(data)
+  }
+
+  useEffect(()=>{
+    getDentist()
+  },[])
  
   // Consumiendo el parametro dinamico de la URL deberan hacer un fetch a un user en especifico
 
@@ -12,8 +26,37 @@ const Detail = () => {
       <h1>Detail Dentist id </h1>
       {/* aqui deberan renderizar la informacion en detalle de un user en especifico */}
       {/* Deberan mostrar el name - email - phone - website por cada user en especifico */}
+      <p>Name: {dentist.name}</p>
+      <p>Email: {dentist.email}</p>
+      <p>Phone: {dentist.phone}</p>
+      <p>Website: {dentist.website}</p>
+    
     </>
   )
 }
+/*
+{
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+      "street": "Kulas Light",
+      "suite": "Apt. 556",
+      "city": "Gwenborough",
+      "zipcode": "92998-3874",
+      "geo": {
+        "lat": "-37.3159",
+        "lng": "81.1496"
+      }
+    },
+    "phone": "1-770-736-8031 x56442",
+    "website": "hildegard.org",
+    "company": {
+      "name": "Romaguera-Crona",
+      "catchPhrase": "Multi-layered client-server neural-net",
+      "bs": "harness real-time e-markets"
+    }
+*/
 
 export default Detail
